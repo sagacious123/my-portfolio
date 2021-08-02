28dbc26c9d0b6225cc49189c249dd38165ba1bcf
bajegbo joseph <damilolaj23@gmail.com>
2021-08-02 21:42:18 +0100

    more updates

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ const navToggler = document.querySelector(".nav-toggler");
 const header = document.querySelector("header");
 const navItems = document.querySelector(".nav-items");
 
-window.onload = () => {
-    setTimeout(() => {
-        document.querySelector("body").classList.add("display")
-    }, 3000);
-}
 
 navToggler.addEventListener("click", toggleNav())
 
@@ -62,3 +57,70 @@ function toggleNav() {
     };
 }
 
+
+// TYPEWRITER EFFECT
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+
+  // window.onload = () => {
+    setTimeout(() => {
+        document.querySelector("body").classList.add("display")
+    }, 3000);
+// }
+
+
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap::after { content: "|"; position: absolute; right: 0; animation: caret 1s infinite; animation-timing-function: steps(1, end); }";
+  document.body.appendChild(css);
+};
