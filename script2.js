7543a9f35c26b44ed21ff6c875ee0426de34e3d0
bajegbo joseph <damilolaj23@gmail.com>
2021-09-19 23:22:28 +0100

    addition of portfolio page

--- script2.js
+++ script2.js
@@ -21,3 +21,19 @@ window.addEventListener('scroll', () => {
     }
 })
 
+const portfolio2 = document.querySelector('.portfolio')
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset + window.innerHeight >= portfolio2.offsetTop + portfolio2.offsetHeight - 1000 && window.innerWidth >= 900) {
+
+        portfolio2.classList.add('change');
+
+    } else if (window.pageYOffset + window.innerHeight >= portfolio2.offsetTop + portfolio2.offsetHeight - 1900 && window.innerWidth < 900) {
+
+        portfolio2.classList.add('change');
+
+    } else {
+        portfolio2.classList.remove('change');
+    }
+})
+
